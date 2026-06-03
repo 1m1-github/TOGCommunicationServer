67b1f0de6836c867a4dfb8509382f067a2992755
1m1 <imi@1m1.io>
2026-06-03 13:02:24 -0500

    3.0.0

--- src/TOGCommunicationServer.jl
+++ src/TOGCommunicationServer.jl
@@ -1,21 +1,46 @@
 module TOGCommunicationServer
 
 using ZMQ
-using LoopOS: listen, @whiletrue
+using LoopOS: @whiletrue
 using TOGZMQ #, TOGZMQClient
 
+# const GROUPS = Dict{String, Vector{String}}("∀" => [""])
 # const GROUP = Dict{String,String}()
-const GROUPS = Set{String}()
+# const GROUPS = Set{String}()
 const ROUTERSOCKET = Ref{Socket}()
 const PUBSOCKET = Ref{Socket}()
 
+# creategroup(;group) = push!(GROUPS, group)
+# rmgroup(;group) = delete!(GROUPS, group)
 # function addclient(;group, name)
 #     to ∈ GROUPS && error("$name already exists as a client")
 #     push!(GROUPS, to)
 #     # TOGZMQClient.start(group=group, router=ROUTERSOCKET[].last_endpoint, pub=PUBSOCKET[].last_endpoint, name=name)
 # end
-
-# todo creategroup, addtogroup, rmfromgroup, rmgroup
+# function creategroup(;groupname, creatorname)
+#     haskey(GROUPS, groupname) || return
+#     GROUPS[groupname] = [creatorname]
+# end
+# function addtogroup(;groupname, user, membername)
+#     haskey(GROUPS, groupname) || return
+#     group = GROUPS[groupname]
+#     user == group[1] || return
+#     membername ∈ values(group) && return
+#     push!(group, membername)
+# end
+# function rmgroup(;groupname, user)
+#     haskey(GROUPS, groupname) || return
+#     user == GROUPS[groupname][1] || return
+#     delete!(GROUPS, groupname)
+# end
+# function rmfromgroup(;groupname, user, membername)
+#     haskey(GROUPS, groupname) || return
+#     group = GROUPS[groupname]
+#     user == group[1] || return
+#     user == membername && return
+#     group = filter!(n -> n ≠ membername, group)
+# end
+# addtogroup(groupname="∀", user=name, membername=name)
 
 function awaken(;router, pub)
     ROUTERSOCKET[] = Socket(ROUTER)
@@ -24,19 +49,21 @@ function awaken(;router, pub)
     bind(PUBSOCKET[], pub)
     # listen(RECEIVE)
     @async @whiletrue begin
-        to, from, symbol, description, information = TOGZMQ.receive(ROUTERSOCKET[])
+        to, togroup, from, symbol, description, information = TOGZMQ.receive(ROUTERSOCKET[])
         # haskey(AGENT, from) || continue
-        TOGZMQ.send(PUBSOCKET[], to, from, information)
+        # TOGZMQ.send(PUBSOCKET[], to, from, information)
         # if to == "group"
-        if to ∈ GROUPS
+        # if to ∈ GROUPS
+        socket = togroup ? PUBSOCKET[] : ROUTERSOCKET[]
+        # if togroup
             # group = AGENTGROUP[from]
-            TOGZMQ.send(PUBSOCKET[], [to, from, symbol, description, information])
+            # TOGZMQ.send(PUBSOCKET[], [to, from, symbol, description, information])
             # send_multipart(PUBSOCKET[], [group, from, message])
         # elseif AGENTGROUP[from] == get(AGENTGROUP, to, "")
-        else
-            TOGZMQ.send(ROUTERSOCKET[], [to, from, symbol, description, information])
+        # else
             # send_multipart(ROUTERSOCKET[], [to, from, message])
-        end
+        # end
+        TOGZMQ.send(socket, to, togroup, from, symbol, description, information)
     end
 end
 
